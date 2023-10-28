a34b970b77e36ee978f1da95361bb86f4f0834f0
Nick Larsen <fodylarsen@gmail.com>
2023-10-27 21:29:29 -0400

    support debugging

--- sat-solver/Program.cs
+++ sat-solver/Program.cs
@@ -1,23 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
+﻿using System.Diagnostics;
 using sat_solver;
 
-if (args.Length == 0)
+bool debugging = Debugger.IsAttached;
+string target = "solve";
+string[] targetArgs = new[] { 
+    "no-copy", 
+    "../problems/2023/cnf/0297c2a35f116ffd5382aea5b421e6df-Urquhart-s3-b3.shuffled-as.sat03-1556.cnf" 
+};
+
+if (!debugging)
 {
-    Console.WriteLine("no args provided, nothing useful to do");
-    return;
+    if (args.Length == 0)
+    {
+        Console.WriteLine("no args provided, nothing useful to do");
+        return;
+    }
+    target = args[0];
+    targetArgs = args.Skip(1).ToArray();
 }
 
-switch(args[0])
+switch(target)
 {
     case "solve":
         var solverProgram = new SolverProgram();
-        solverProgram.Run(args.Skip(1).ToArray());
+        solverProgram.Run(targetArgs);
         break;
     case "inspect":
         var inspectorProgram = new InspectorProgram();
-        inspectorProgram.Run(args.Skip(1).ToArray());
+        inspectorProgram.Run(targetArgs);
         break;
     default:
         Console.WriteLine($"Unknown task provided: {args[0]}, please try again.");
