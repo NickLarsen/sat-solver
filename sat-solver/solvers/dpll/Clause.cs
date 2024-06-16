bfe62185382cd8d87c7a7e9b7a1d661089719c26
Nick Larsen <fodylarsen@gmail.com>
2024-06-16 17:07:53 -0400

    delete DPLLSolver

--- sat-solver/solvers/dpll/Clause.cs
+++
@@ -1,8 +0,0 @@
-namespace sat_solver.solvers.dpll;
-
-class Clause
-{
-    public required int[] Literals { get; set; }
-    public bool IsEmptyClause => Literals.Length == 0;
-    public bool IsUnitClause => Literals.Length == 1;
-}
